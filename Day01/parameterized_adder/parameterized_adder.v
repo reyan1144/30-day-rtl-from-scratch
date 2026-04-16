75d1b3687f7b25b04dbebd4b95c5e6eb03370081
reyan1144 <mdreyan7771@gmail.com>
2026-04-16 13:09:01 +0530

    Add parameterized adder module for N-bit addition

---
+++ Day01/parameterized_adder/parameterized_adder.v
@@ -0,0 +1,21 @@
+`timescale 1ns/1ps   // Time unit = 1ns, precision = 1ps
+
+// Parameterized Adder Module
+// Adds two N-bit inputs (WIDTH can be changed)
+// Produces N-bit SUM and 1-bit carry-out
+
+module parameterized_adder #(parameter WIDTH = 16)
+(
+    input  [WIDTH-1:0] A,   // First input operand (N-bit)
+    input  [WIDTH-1:0] B,   // Second input operand (N-bit)
+    output [WIDTH-1:0] SUM, // Result of addition (N-bit)
+    output COUT             // Carry-out (1-bit)
+);
+
+// Perform addition
+// {COUT, SUM} is a concatenation:
+//  COUT gets the MSB (overflow bit)
+//  SUM gets the remaining WIDTH bits
+assign {COUT, SUM} = A + B;
+
+endmodule
